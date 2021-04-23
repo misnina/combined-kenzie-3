14981bc3218610724f8ddb6fdb627a9d4ca206c2
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 15:16:18 -0500

    making sure the front end runs

--- frontend/src/fetchRequests.js
+++ frontend/src/fetchRequests.js
@@ -1,4 +1,4 @@
-const baseURL = process.env.public_url || "https://infinite-beach-71989.herokuapp.com/";
+const baseURL = `https://${window.location.host}/`;
 
 export const getUser = function (authorid) { 
   return fetch(baseURL + `user/${authorid.toString()}`)
