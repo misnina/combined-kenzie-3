ab0e282e5706096e168036f34774ec24003150f6
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 02:49:17 -0500

    maybe?

--- frontend/src/service/socket.js
+++ frontend/src/service/socket.js
@@ -1,7 +1,5 @@
 import io from "socket.io-client";
 import React from 'react';
 
-const PORT = process.env.PORT || 4000;
-const url = ("ws://" + window.location.host);
-export const socket = io.connect(url);
+export const socket = io.connect();
 export const SocketContext = React.createContext(socket);
