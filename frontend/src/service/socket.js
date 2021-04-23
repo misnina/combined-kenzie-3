395e66838077f61c9caef14a4ae5b3dec48468e0
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 01:54:12 -0500

    added port to websocket

--- frontend/src/service/socket.js
+++ frontend/src/service/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 import React from 'react';
 
-export const socket = io.connect(process.env.public_url, { transports : ['websocket'] });
+export const socket = io.connect(`${process.env.public_url}:${process.env.PORT}`, { transports : ['websocket'] });
 export const SocketContext = React.createContext(socket);
