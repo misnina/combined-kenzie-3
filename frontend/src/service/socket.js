a395bee9699f7ec31a864a3110f6ec012bd265ba
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 02:59:37 -0500

    websocket transport

--- frontend/src/service/socket.js
+++ frontend/src/service/socket.js
@@ -2,5 +2,5 @@ import io from "socket.io-client";
 import React from 'react';
 
 const url = ("ws://" + window.location.host);
-export const socket = io.connect(url);
+export const socket = io.connect(url, { transports : ['websocket'] });
 export const SocketContext = React.createContext(socket);
