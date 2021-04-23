a73dcdd372c4c88ada7b7dcba8277701e3194aca
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 02:08:06 -0500

    would this actually work?

--- frontend/src/service/socket.js
+++ frontend/src/service/socket.js
@@ -1,9 +1,5 @@
 import io from "socket.io-client";
 import React from 'react';
 
-const url = process.env.public_url.toString();
-const port = process.env.PORT.toString();
-console.log(typeof process.env.public_url);
-
-export const socket = io.connect(`${url}:${port}`, { transports : ['websocket'] });
+export const socket = io();
 export const SocketContext = React.createContext(socket);
