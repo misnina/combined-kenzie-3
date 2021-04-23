0f8a3ef7c934cd6e7c133060c0532d9e1dff2e33
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 02:20:23 -0500

    bah

--- frontend/src/service/socket.js
+++ frontend/src/service/socket.js
@@ -1,5 +1,7 @@
-import { io } from "../../../backend/index.js";
+import io from "socket.io-client";
 import React from 'react';
 
-export const socket = io();
+console.log(process.env.PORT);
+
+export const socket = io.connect(("ws://" + window.location.href + ":" + process.env.PORT), { transports : ['websocket'] });
 export const SocketContext = React.createContext(socket);
